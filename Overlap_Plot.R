d3ee8256b8107e1e4452f5058da0a0406b285fd8
AlysonEast <a.east.ecology@gmail.com>
2026-06-22 11:08:15 -0400

    Overlap quantification across scale

---
+++ Overlap_Plot.R
@@ -0,0 +1,520 @@
+library(ggplot2)
+library(dplyr)
+library(ggplot2)
+library(sf)
+library(maps)
+library(dplyr)
+library(ggrepel)
+
+# getwd()
+# setwd("/home/aly/Beetles/BeetleBodySizeVariation")
+# df<-read.csv("./BeetleMeasurements.csv")
+# meta<-read.csv("./NEON_Field_Site_Metadata_20260130.csv")
+# puum<-read.csv("./trait_annotations.csv")
+# 
+# df<-merge(df, meta, by.x="siteID", by.y="site_id", all.x=TRUE)
+# 
+# head(df)
+# 
+# df_sub<-subset(df, user_name=="IsaFluck")
+# 
+# head(df_sub)
+# str(df_sub)
+# 
+# ElytraLength<-subset(df_sub, structure=="ElytraLength")
+# 
+# hist(ElytraLength$dist_cm)
+# 
+# site_spp<-table(ElytraLength$species, ElytraLength$siteID)
+# site_spp<-as.data.frame(site_spp)
+# 
+# ElytraLength<-subset(ElytraLength, siteID!="DELA" & siteID!="DSNY")
+
+#### READ IN DATA####
+
+df<-read.csv("./Data/beetle_lengths_cm_reviewed_clean.csv")
+allInd<-read.csv("./Data/allIndividuals.csv")
+
+head(df)
+str(df)
+biorepo<-merge(df, allInd, by.x="beetle_id", by.y = "individualID", all.x = TRUE)
+table(biorepo$flag)
+biorepo<-subset(biorepo, is.na(flag))
+
+#Update with new data, this is data specific. 
+biorepo <- biorepo %>% 
+  group_by(beetle_id) %>% 
+  slice_head(n = 1) %>% 
+  ungroup()
+
+biorepo$cm_elytra_max_length<-biorepo$length_cm
+
+#
+
+biorepo$individualID<-biorepo$beetle_id
+biorepo$datasource<-"Biorepo"
+
+sp_count <- biorepo %>%
+  group_by(scientific_name) %>%
+  summarise(n = n()) %>%
+  ungroup()
+
+#Add in Beetlepalooza annotations
+paloozadf<-read.csv("./Data/BeetleMeasurements.csv")
+meta<-read.csv("./Data/NEON_Field_Site_Metadata_20260130.csv")
+
+paloozadf<-merge(paloozadf, meta, by.x="siteID", by.y="site_id", all.x=TRUE)
+df_sub<-subset(paloozadf, user_name=="IsaFluck")
+
+ElytraLength<-subset(df_sub, structure=="ElytraLength")
+colnames(ElytraLength)
+ElytraLength$cm_elytra_max_length<-ElytraLength$dist_cm
+ElytraLength$datasource<-"Beetle Palooza"
+head(ElytraLength)
+
+library(tidyr)
+library(dplyr)
+library(stringr)
+library(ggpubr)
+
+ElytraLength <- ElytraLength %>%
+  mutate(
+    sample_date = str_extract(NEON_sampleID, "\\d{8}"),
+    collectDate = as.Date(sample_date, format = "%Y%m%d"),
+    yearCollected = as.integer(substr(sample_date, 1, 4))
+  )
+#Add in Hawaii data ####
+HI<-read.csv("./Data/trait_annotations.csv")
+colnames(HI)
+HI$datasource<-"PUUM"
+
+#### Load NEON Token and Data Product ####
+# Read NEON token from file
+neon_token <- read.delim("~/NEON_TOKEN", header = FALSE)[1, 1]
+Beetle_dpID <- "DP1.10022.001"
+
+# If already combined data exists, load it, else fetch and combine
+if (file.exists("./Data/NEON_ExpertParaCombined.csv")) {
+  combined_data <- read.csv("./Data/NEON_ExpertParaCombined.csv")
+} else {
+  neon_df <- neonUtilities::loadByProduct(
+    dpID = Beetle_dpID,
+    token = neon_token,
+    site = "PUUM",
+    include.provisional = FALSE,
+    check.size = FALSE
+  )
+  
+  neon_para <- neon_df$bet_parataxonomistID
+  neon_expert <- neon_df$bet_expertTaxonomistIDProcessed
+  
+  neon_para_clean <- neon_para %>%
+    filter(!(individualID %in% neon_expert$individualID))
+  
+  common_cols <- intersect(names(neon_para_clean), names(neon_expert))
+  neon_para_common <- neon_para_clean[, common_cols]
+  neon_expert_common <- neon_expert[, common_cols]
+  
+  neon_para_common$ID_status <- "Para"
+  neon_expert_common$ID_status <- "Expert"
+  
+  combined_data <- bind_rows(neon_para_common, neon_expert_common)
+  combined_data$numbericID <- as.numeric(substr(combined_data$individualID, 
+                                                (nchar(combined_data$individualID) - 5), 
+                                                nchar(combined_data$individualID)))
+  
+  write.csv(combined_data, "./Data/NEON_ExpertParaCombined.csv", row.names = FALSE)
+}
+
+if (file.exists("./Data/NEON_ExpertParaCombined_Prelim.csv")) {
+  combined_data_prelim <- read.csv("./Data/NEON_ExpertParaCombined_Prelim.csv")
+} else {
+  neon_df <- neonUtilities::loadByProduct(
+    dpID = Beetle_dpID,
+    token = neon_token,
+    include.provisional = TRUE,
+    site = "PUUM",
+    check.size = FALSE
+  )
+  
+  neon_para <- neon_df$bet_parataxonomistID
+  neon_expert <- neon_df$bet_expertTaxonomistIDProcessed
+  
+  neon_para_clean <- neon_para %>%
+    filter(!(individualID %in% neon_expert$individualID))
+  
+  common_cols <- intersect(names(neon_para_clean), names(neon_expert))
+  neon_para_common <- neon_para_clean[, common_cols]
+  neon_expert_common <- neon_expert[, common_cols]
+  
+  neon_para_common$ID_status <- "Para"
+  neon_expert_common$ID_status <- "Expert"
+  
+  combined_data_prelim <- bind_rows(neon_para_common, neon_expert_common)
+  combined_data_prelim$numbericID <- as.numeric(substr(combined_data_prelim$individualID, 
+                                                       (nchar(combined_data_prelim$individualID) - 5), 
+                                                       nchar(combined_data_prelim$individualID)))
+  
+  write.csv(combined_data_prelim, "./Data/NEON_ExpertParaCombined_Prelim.csv", row.names = FALSE)
+}
+
+combined_data_prelim<-subset(combined_data_prelim, release=="PROVISIONAL")
+
+dim(combined_data)
+combined_data<-rbind(combined_data, combined_data_prelim)
+dim(combined_data)
+
+# Add year to combined_data
+combined_data$yearCollected <- as.numeric(substr(combined_data$collectDate, 1, 4))
+
+# Extract genus and species only from scientific name
+combined_data$scientificName_Species<-gsub(r"{\s*\([^\)]+\)}","",as.character(combined_data$scientificName))
+combined_data$scientificName_Species<-gsub(" {2,}", " ", combined_data$scientificName_Species)
+combined_data$scientificName_Species<-sub("^(\\S*\\s+\\S+).*", "\\1", combined_data$scientificName_Species)
+
+combined_data$scientificName_Species <-
+  gsub("/.*$", "", combined_data$scientificName_Species)
+
+#Add metadata to PUUM dataset
+HI_meta<-merge(HI, combined_data, by="individualID", all.x = TRUE)
+
+# Merge all data
+EL_harm <- ElytraLength %>%
+  transmute(
+    datasource,
+    siteID,
+    plotID,
+    domainID = domain_id,
+    individualID = combinedID,   # probably better than NEON_sampleID
+    sampleID = NEON_sampleID,
+    scientificName,
+    collectDate,
+    yearCollected,
+    imageID = pictureID,
+    cm_elytra_max_length,
+    latitude
+  )
+
+HI_harm <- HI_meta %>%
+  transmute(
+    datasource,
+    siteID,
+    plotID,
+    domainID,
+    individualID,
+    scientificName,
+    imageID = groupImageFilePath,
+    px_scalebar,
+    cm_scalebar,
+    px_elytra_max_length,
+    cm_elytra_max_length,
+    yearCollected
+  )
+
+biorepo_harm  <- biorepo %>%
+  transmute(
+    datasource,
+    siteID,
+    plotID,
+    domainID,
+    individualID,
+    scientificName,
+    imageID,
+    px_scalebar = NA, ##Update!!!!
+    cm_scalebar = NA, ##Update!!!!
+    px_elytra_max_length = NA, ##Update!!!!
+    cm_elytra_max_length,
+    yearCollected
+  )
+
+#Add in biorepo
+all_elytra <- bind_rows(EL_harm, HI_harm, biorepo_harm)
+
+#### Clean the data####
+all_elytra<-subset(all_elytra, !cm_elytra_max_length<=0)
+
+# Extract genus and species only from scientific name
+all_elytra$scientificName_Species<-gsub(r"{\s*\([^\)]+\)}","",as.character(all_elytra$scientificName))
+all_elytra$scientificName_Species<-gsub(" {2,}", " ", all_elytra$scientificName_Species)
+all_elytra$scientificName_Species<-sub("^(\\S*\\s+\\S+).*", "\\1", all_elytra$scientificName_Species)
+
+all_elytra$scientificName_Species <-
+  gsub("/.*$", "", all_elytra$scientificName_Species)
+all_elytra<-subset(all_elytra, !is.na(scientificName_Species))
+
+all_elytra <- all_elytra %>%
+  filter(!grepl("sp\\.", scientificName_Species))
+
+all_elytra<-subset(all_elytra, yearCollected == 2018 | yearCollected == 2019)
+
+#### Explore the data ####
+# Get unique siteID levels ordered by latitude
+site_order <- unique(all_elytra[order(all_elytra$latitude, decreasing = TRUE), "siteID"])
+
+# Convert siteID into a factor with the correct order
+all_elytra$siteID <- factor(all_elytra$siteID, levels = site_order)
+all_elytra$log_dist_cm<-log10(all_elytra$cm_elytra_max_length)
+
+library(ggpubr)
+table(subset(all_elytra, siteID=="STER")$plotID)
+sort(table(subset(all_elytra, siteID=="STER")$scientificName_Species))
+table(subset(all_elytra, siteID=="STER")$scientificName_Species, subset(all_elytra, siteID=="STER")$plotID)
+
+all_elytra_plot50 <- all_elytra %>%
+  group_by(plotID, scientificName_Species) %>%
+  filter(n() >= 20) %>%
+  ungroup()
+all_elytra_site50 <- all_elytra %>%
+  group_by(siteID, scientificName_Species) %>%
+  filter(n() >= 20) %>%
+  ungroup()
+
+ggarrange(nrow=4,
+          ggplot(data = subset(all_elytra, siteID=="STER"), aes(x=log_dist_cm, fill = scientificName_Species)) +
+            geom_density(alpha=0.5) +
+            #  theme(legend.position="none") +
+            facet_wrap(.~siteID, scales = "free_y"),
+          ggplot(data = subset(all_elytra, siteID=="STER"), aes(x=log_dist_cm, fill = scientificName_Species)) +
+            geom_density(alpha=0.5) +
+            #  theme(legend.position="0") +
+            facet_wrap(.~plotID, scales = "free_y"),
+          ggplot(data = subset(all_elytra_site50, siteID=="STER"), aes(x=log_dist_cm, fill = scientificName_Species)) +
+            geom_density(alpha=0.5) +
+            #  theme(legend.position="none") +
+            facet_wrap(.~siteID, scales = "free_y"),
+          ggplot(data = subset(all_elytra_plot50, siteID=="STER"), aes(x=log_dist_cm, fill = scientificName_Species)) +
+            geom_density(alpha=0.5) +
+            #  theme(legend.position="0") +
+            facet_wrap(.~plotID, scales = "free_y")
+)
+
+#### OStats for the site level
+#### Overlap Stats ####
+library(Ostats)
+
+plot20plus_elytraDF<-all_elytra_plot50[,c("domainID","siteID","plotID","scientificName_Species","log_dist_cm")]
+
+Ostats_20plus <- Ostats(traits = as.matrix(plot20plus_elytraDF[,'log_dist_cm', drop = FALSE]),
+                         sp = factor(plot20plus_elytraDF$scientificName_Species),
+                         plots = factor(plot20plus_elytraDF$plotID),
+                         random_seed = 517)
+
+as.data.frame(Ostats_20plus$overlaps_norm)
+Ostats_20plus$overlaps_norm_ses
+
+plots<-sort(unique(plot20plus_elytraDF$plotID))
+plots
+subplots<-as.data.frame(plots)
+subplots$siteID<-substr(subplots$plots, 1, 4)
+sites<-unique(subplots$siteID)
+table(subplots$siteID)
+
+
+for (i in 1:length(sites)) {
+  png(paste0("./Figures/Overlap/Plots/PlotOverlap",sites[i],"20Plus.png"), units = "in", width = 9, height = 5, res=300)
+  print(Ostats_plot(plots = plot20plus_elytraDF$plotID,
+                    sp = plot20plus_elytraDF$scientificName_Species,
+                    traits = plot20plus_elytraDF$log_dist_cm,
+                    overlap_dat = Ostats_20plus,
+                    use_plots = subset(subplots, siteID==sites[i])$plots,
+                    name_x = 'log10(Elytra Length (cm))',
+                    means = FALSE,
+                    n_col = 5,
+                    scale = "free_y"))
+  dev.off()
+}
+
+
+#### Map of overlaps ####
+library(neonDivData)
+neon_location_BET<-subset(neon_location, substr(location_id, (nchar(location_id)-2), nchar(location_id))=="bet")
+
+Ostats_map<-merge(neon_location_BET, Ostats_20plus$overlaps_norm, by.x="plotID", by.y = "row.names")
+Ostats_map <- Ostats_map %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
+
+# Convert state map to dataframe
+states_map <- map_data("state")
+
+ggplot() + geom_polygon(data = states_map, aes(x = long, y = lat, group = group),   # Base map
+                        fill = "gray95",
+                        color = "gray70",
+                        linewidth = 0.2) +
+  geom_sf(data = Ostats_map, aes(color = log_dist_cm),   # Site points
+          size = 6, alpha = 0.9, inherit.aes = FALSE) +
+  geom_text_repel(data = Ostats_map, aes(label = plotID, geometry = geometry), # Site labels
+                  stat = "sf_coordinates", size = 3,
+                  min.segment.length = 0, segment.color = NA,
+                  seed = 42, inherit.aes = FALSE) +
+  scale_color_viridis_c(name = "Overlap", option = "magma") +
+  coord_sf() +
+  theme_minimal(base_size = 12) +
+  theme(panel.grid = element_blank(),
+        legend.position = "right") +
+  labs(title = "Site Locations",
+       subtitle = "Points colored by overlap",
+       x = NULL,
+       y = NULL)
+
+#### Average distribution shape ####
+# -------------------------------------------------------------------------
+# SIMULATE ADDITIONAL OBSERVATIONS FOR LOW-SAMPLE SPECIES-SITE COMBINATIONS
+#
+# Goal:
+#   Increase all species-site combinations to a minimum sample size of 20
+#   using an empirically-derived variance relationship.
+#
+# Assumptions:
+#   1. Well-sampled species-site combinations provide a representative
+#      estimate of within-group variability.
+#
+#   2. Typical variability is described by:
+#
+#         cv2_pct = 100 * variance / mean^2
+#
+#      where cv2_pct is the average observed value across high-sample groups.
+#
+#   3. Elytra lengths are positive continuous measurements, so simulated
+#      values are drawn from a lognormal distribution rather than a normal
+#      distribution.
+#
+#      This prevents impossible negative lengths and preserves the
+#      observed mean-variance scaling.
+# -------------------------------------------------------------------------
+# Average CV² (%) estimated from well-sampled species-site combinations
+typical_cvpct <- 0.47
+
+# Convert percentage to proportion
+cv2 <- typical_cvpct / 100
+
+# -------------------------------------------------------------------------
+# Identify low-sample species-site combinations
+# -------------------------------------------------------------------------
+
+low_n <- all_elytra %>%
+  group_by(scientificName_Species, plotID) %>%
+  summarise(
+    n_obs = n(),
+    mean_dist = mean(cm_elytra_max_length, na.rm = TRUE),
+    .groups = "drop"
+  ) %>%
+  filter(n_obs < 20)
+
+# -------------------------------------------------------------------------
+# Simulate observations
+# -------------------------------------------------------------------------
+#
+# For a lognormal distribution:
+#
+#   CV² = exp(sdlog²) - 1
+#
+# therefore:
+#
+#   sdlog = sqrt(log(1 + CV²))
+#
+# and
+#
+#   meanlog = log(mean) - sdlog² / 2
+#
+# This parameterization ensures that the simulated observations have:
+#
+#   E[X] = observed mean
+#
+# and approximately:
+#
+#   Var[X] / E[X]² = CV²
+#
+# -------------------------------------------------------------------------
+
+set.seed(42)
+
+sim_low_n <- low_n %>%
+  rowwise() %>%
+  mutate(n_to_add = 20 - n_obs, # Number of observations needed to reach n = 20
+         sdlog = sqrt(log(1 + cv2)), # Lognormal parameters implied by the typical CV² relationship
+         meanlog = log(mean_dist) - (sdlog^2 / 2),
+         # Simulate additional observations
+         sim_vals = list(rlnorm(n = n_to_add,
+                                meanlog = meanlog,
+                                sdlog = sdlog))) %>%
+  unnest(cols = sim_vals) %>%
+  rename(cm_elytra_max_length = sim_vals) %>%
+  select(scientificName_Species,
+         plotID,
+         cm_elytra_max_length) %>%
+  ungroup()
+
+sim_low_n$siteID<-NA
+sim_low_n$domainID<-NA
+
+all_elytraDF<-all_elytra[,c("domainID","siteID","plotID","scientificName_Species","cm_elytra_max_length")]
+head(all_elytraDF)
+head(sim_low_n)
+
+all_elytra_aug <- rbind(all_elytraDF, sim_low_n[,colnames(all_elytraDF)])
+all_elytra_aug$log_dist_cm<-log10(all_elytra_aug$cm_elytra_max_length)
+
+Ostats_aug <- Ostats(traits = as.matrix(all_elytra_aug[,'log_dist_cm', drop = FALSE]),
+                         sp = factor(all_elytra_aug$scientificName_Species),
+                         plots = factor(all_elytra_aug$plotID),
+                         random_seed = 517)
+
+hist(Ostats_20plus$overlaps_norm)
+hist(Ostats_aug$overlaps_norm)
+
+
+plots<-sort(unique(all_elytra_aug$plotID))
+plots
+subplots<-as.data.frame(plots)
+subplots$siteID<-substr(subplots$plots, 1, 4)
+sites<-unique(subplots$siteID)
+table(subplots$siteID)
+
+
+for (i in 1:length(sites)) {
+  png(paste0("./Figures/Overlap/Plots/PlotOverlap",sites[i],"Augmented.png"), units = "in", width = 9, height = 5, res=300)
+  print(Ostats_plot(plots = all_elytra_aug$plotID,
+                    sp = all_elytra_aug$scientificName_Species,
+                    traits = all_elytra_aug$log_dist_cm,
+                    overlap_dat = Ostats_aug,
+                    use_plots = subset(subplots, siteID==sites[i])$plots,
+                    name_x = 'log10(Elytra Length (cm))',
+                    means = FALSE,
+                    n_col = 5,
+                    scale = "free_y"))
+  dev.off()
+  }
+
+
+
+neon_location_BET<-subset(neon_location, substr(location_id, (nchar(location_id)-2), nchar(location_id))=="bet")
+
+Ostats_aug_map<-merge(neon_location_BET, Ostats_aug$overlaps_norm, by.x="plotID", by.y = "row.names")
+Ostats_aug_map <- Ostats_aug_map %>%
+  st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
+
+ggplot() + geom_polygon(data = states_map, aes(x = long, y = lat, group = group),   # Base map
+                        fill = "gray95",
+                        color = "gray70",
+                        linewidth = 0.2) +
+  geom_sf(data = subset(Ostats_aug_map), aes(color = log_dist_cm),   # Site points
+          size = 6, alpha = 0.9, inherit.aes = FALSE) +
+  geom_text_repel(data = subset(Ostats_aug_map), aes(label = plotID, geometry = geometry), # Site labels
+                  stat = "sf_coordinates", size = 3,
+                  min.segment.length = 0, segment.color = NA,
+                  seed = 42, inherit.aes = FALSE) +
+  scale_color_viridis_c(name = "Overlap", option = "magma") +
+  coord_sf() +
+  theme_minimal(base_size = 12) +
+  theme(panel.grid = element_blank(),
+        legend.position = "right") +
+  labs(title = "Site Locations",
+       subtitle = "Points colored by overlap",
+       x = NULL,
+       y = NULL)
+ 
+#### Write out data 
+write.csv(Ostats_aug_map, "./Outputs/Plot_Ostats_augmented.csv", row.names = FALSE)
+write.csv(Ostats_map, "./Outputs/Plot_Ostats_20plus.csv", row.names = FALSE)
+
+
